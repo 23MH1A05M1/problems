2cdf60992565fef92b1c7514a12df785ed2d87dc
Bhagyasri <147595512+23MH1A05M1@users.noreply.github.com>
2025-10-13 22:44:34 +0530

    Time: 2 ms (63.39%), Space: 43.1 MB (62.93%) - LeetHub

---
+++ 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int res=nums2[0]-nums1[0];
+        
+        // for(int i=0;i<nums1.length;i++){
+
+        // }
+        return res;
+    }
+}
