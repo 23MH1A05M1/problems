911185bb72497a9025f24e9438f3d1a19e8833d9
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-06-23 22:04:06 +0530

    Time: 7 ms (8.18%), Space: 44.8 MB (6.08%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int secondHighest(String s) {
+        s = s.replaceAll("[a-z]", "");
+        int[] arr = new int[(s.length())];
+
+        for (int i = 0; i < s.length(); i++) {
+            arr[i] = s.charAt(i) - '0';
+        }
+        if(arr.length == 0){
+            return -1;
+        }
+        int large = arr[0];
+        int slarge = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > large) {
+                slarge = large;
+                large = arr[i];
+            } else if (arr[i] < large && arr[i] > slarge) {
+                slarge = arr[i];
+            }
+        }
+
+        return slarge;
+
+    }
+}
