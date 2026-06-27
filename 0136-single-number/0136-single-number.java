45512dfcdccc4a700f82377303eddce9728c1215
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-06-27 22:23:01 +0530

    Time: 13 ms (19.73%), Space: 47 MB (27.68%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int count = 0;
-        int result=0;
-        for(int i=0;i<nums.length;i++)
-        {  
-            result = result^nums[i];
-        }
-        return result;
+       Map<Integer,Integer>map = new HashMap<>();
+
+      for(int num:nums){
+        map.put(num,map.getOrDefault(num,0)+1);
+      }
+      int res=0;
+   for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+    if(entry.getValue()==1){
+         res=entry.getKey();
     }
+   }
+     return res;
+}
 }
