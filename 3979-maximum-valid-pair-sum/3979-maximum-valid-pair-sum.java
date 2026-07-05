b1ed683a4c6db4d12d4769a61ad4fd475a57ffcc
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-07-05 22:08:19 +0530

    Time: 1 ms (100%), Space: 119.1 MB (100%) - LeetHub

---
+++ 3979-maximum-valid-pair-sum/3979-maximum-valid-pair-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxValidPairSum(int[] nums, int k) {
+        int sum=0;
+        int max=nums[0];
+        int res=Integer.MIN_VALUE;
+        
+        for(int i=k;i<nums.length;i++){
+           max = Math.max(max,nums[i-k]);
+            res = Math.max(res,max+nums[i]);
+        }
+        return res;
+    }
+}
