c1ea467acdde785172f04870889ff6027000c6b4
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-07-21 22:07:11 +0530

    Time: 5 ms (5.16%), Space: 43.2 MB (68%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,21 +1,35 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length==0 || strs==null){
-            return "";
-        }
-       String first=strs[0];
-       for(int i=0;i<first.length();i++){
-        char ch = first.charAt(i);
-        for(int j=1;j<strs.length;j++){
-            if(i>=strs[j].length()){
-                return first.substring(0,i);
-            }
-            if(ch!=strs[j].charAt(i)){
-                return first.substring(0,i);
-            }
-        }
+    //     if(strs.length==0 || strs==null){
+    //         return "";
+    //     }
+    //    String first=strs[0];
+    //    for(int i=0;i<first.length();i++){
+    //     char ch = first.charAt(i);
+    //     for(int j=1;j<strs.length;j++){
+    //         if(i>=strs[j].length()){
+    //             return first.substring(0,i);
+    //         }
+    //         if(ch!=strs[j].charAt(i)){
+    //             return first.substring(0,i);
+    //         }
+    //     }
 
+    //    }
+    //    return first;
+
+     String result="";
+     Arrays.sort(strs);
+       char[] first = strs[0].toCharArray();
+       char[] last = strs[strs.length-1].toCharArray();
+       for(int i=0;i<first.length;i++){
+        if(first[i]==last[i]){
+            result+=first[i];
+        }
+        else{
+            break;
+        }
        }
-       return first;
+       return result.toString();
     }
 }
