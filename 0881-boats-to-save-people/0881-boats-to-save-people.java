6bfd930a5bc1f0a5cb1569944899886b48ec671c
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-07-24 22:55:06 +0530

    Time: 19 ms (98.6%), Space: 56.7 MB (20.94%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -5,26 +5,38 @@ class Solution {
         int boat=0;
         Arrays.sort(people);
         while(i<=j){
-            if(people[i]+people[j]>limit){
-                if(people[i]==limit){
-                    boat++;
-                    i++;
-                }
-                else{
-                 boat++;
-                 j--;
-                }
-            }
-            else if(people[i]+people[j]==limit || people[i]+people[j]<limit){
-                boat++;
+
+             if(people[i]+people[j]<=limit){
+                // boat++;
                 i++;
                 j--;
             }
-            if(i==j ){
-                // count++;
-                boat++;
-                break;
+            else{
+                j--;
             }
+            boat++;
+            // if(people[i]+people[j]>limit){
+            //     if(people[i]==limit){
+            //         boat++;
+            //         i++;
+            //     }
+            //     else{
+            //      boat++;
+            //      j--;
+            //     }
+            // }
+            // else if(people[i]+people[j]==limit || people[i]+people[j]<limit){
+            //     boat++;
+            //     i++;
+            //     j--;
+            // }
+            // if(i==j ){
+            //     // count++;
+            //     boat++;
+            //     break;
+            // }
+
+
         }
         return boat;
     }
