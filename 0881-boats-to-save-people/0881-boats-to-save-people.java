1e40f64918aa4e67343c3fbe27f0a0a9846dc144
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-07-24 22:47:41 +0530

    Time: 21 ms (24.85%), Space: 56.8 MB (9.5%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int i=0;
+        int j=people.length-1;
+        int boat=0;
+        Arrays.sort(people);
+        while(i<=j){
+            if(people[i]+people[j]>limit){
+                if(people[i]==limit){
+                    boat++;
+                    i++;
+                }
+                else{
+                 boat++;
+                 j--;
+                }
+            }
+            else if(people[i]+people[j]==limit || people[i]+people[j]<limit){
+                boat++;
+                i++;
+                j--;
+            }
+            if(i==j ){
+                // count++;
+                boat++;
+                break;
+            }
+        }
+        return boat;
+    }
+  
+}
