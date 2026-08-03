6c019a73fff9af46fdfe23d76c8d805865fee07e
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-08-03 15:29:10 +0530

    Time: 7 ms (72.33%), Space: 49.7 MB (58.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,16 +2,27 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> Anagrams = new ArrayList<>();
         HashMap<String,List<String>>map = new HashMap<>();
+        // for(String s : strs){
+        //     char[] ch = s.toCharArray();
+        //     Arrays.sort(ch);
+        //     String sorted = new String(ch);
+        //     map.putIfAbsent(sorted,new ArrayList<>());
+        //     map.get(sorted).add(s);
+        // }
+        // for(Map.Entry<String,List<String>>entry:map.entrySet()){
+        //     Anagrams.add(entry.getValue());
+        // }
+        // return Anagrams;
+
         for(String s : strs){
             char[] ch = s.toCharArray();
             Arrays.sort(ch);
-            String sorted = new String(ch);
-            map.putIfAbsent(sorted,new ArrayList<>());
+            String sorted=new String(ch);
+            if(!map.containsKey(sorted)){
+                map.put(sorted,new ArrayList<>());
+            }
             map.get(sorted).add(s);
         }
-        for(Map.Entry<String,List<String>>entry:map.entrySet()){
-            Anagrams.add(entry.getValue());
-        }
-        return Anagrams;
+        return new ArrayList<>(map.values());
     }
 }
