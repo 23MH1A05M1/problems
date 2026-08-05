3cd21f4d108d325fb4dad92f2a7a925c1280d3d9
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-08-05 13:41:43 +0530

    Time: 49 ms (18.86%), Space: 96.9 MB (5.12%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,13 +1,34 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        PriorityQueue<Integer>p = new PriorityQueue<>();
-        for(int num:nums){
-            p.add(num);
+        // PriorityQueue<Integer>p = new PriorityQueue<>();
+        // for(int num:nums){
+        //     p.add(num);
+        // }
+        // int i=0;
+        // while(i<nums.length){
+        //     nums[i]=p.poll();
+        //     i++;
+        // }
+        // return nums;
+        HashMap<Integer,Integer>map = new HashMap<>();
+        int min=nums[0];
+        int max=nums[0];
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            if(nums[i]>max){
+                max=nums[i];
+            }
+            if(nums[i]<min){
+                min=nums[i];
+            }
         }
-        int i=0;
-        while(i<nums.length){
-            nums[i]=p.poll();
-            i++;
+        int index=0;
+        for(int i=min;i<=max;i++){
+            while(map.getOrDefault(i,0)>0){
+                  nums[index]=i;
+                  index++;
+                  map.put(i,map.get(i)-1);
+            } 
         }
         return nums;
     }
