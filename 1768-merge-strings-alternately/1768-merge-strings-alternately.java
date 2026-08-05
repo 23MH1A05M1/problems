29fd1654bc4ffd00e997f46307fcbce6f3c454da
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-08-05 11:15:09 +0530

    Time: 4 ms (19.06%), Space: 43.9 MB (13.46%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,16 +1,31 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder sb = new StringBuilder();
         int i=0;
-        while(i<word1.length()||i<word2.length()){
-            if(i<word1.length()){
-                sb.append(word1.charAt(i));
-            }
-            if(i<word2.length()){
-                sb.append(word2.charAt(i));
-            }
-            i++;
+        int j=0;
+        String res="";
+        while(i<word1.length() && j<word2.length()){
+          res+=word1.charAt(i);
+           res+=word2.charAt(j);
+           i++;
+           j++;    
         }
-        return sb.toString();
+         if(word1.length()<word2.length()){
+            while(j<word2.length()){
+            if(i==word1.length()){
+            res+=word2.charAt(j);
+            j++;
+           }
+            }
+        }
+        else if(word2.length()<word1.length()){
+             while(i<word1.length()){
+            if(j==word2.length()){
+            res+=word1.charAt(i);
+            i++;
+           }
+             }
+        }
+         
+        return res;
     }
 }
