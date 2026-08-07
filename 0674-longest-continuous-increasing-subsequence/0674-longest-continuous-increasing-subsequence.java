135e48ebb878152926d3246c4ff47a359d101815
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-08-07 14:35:06 +0530

    Time: 2 ms (45.4%), Space: 46.7 MB (74.4%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
@@ -1,10 +1,10 @@
 class Solution {
     public int findLengthOfLCIS(int[] nums) {
         int count=1;
-        int max=0;
-        if(nums.length==1){
-            return 1;
-        }
+        int max=1;
+        // if(nums.length==1){
+        //     return 1;
+        // }
         for(int i=0;i<nums.length-1;i++){
            if(nums[i]<nums[i+1]){
                count++;
