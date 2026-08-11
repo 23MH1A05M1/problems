1a9365393cb82f7436b25b2a1845d35554b80ab5
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-08-11 21:26:42 +0530

    Time: 85 ms (45.82%), Space: 96.1 MB (70.72%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int[][] cars = new int[position.length][2];
+        for(int i=0;i<position.length;i++){
+            cars[i][0]=position[i];
+            cars[i][1]=speed[i];
+        }
+        Stack<Double>stack=new Stack<>();
+        Arrays.sort(cars,(a,b)->Integer.compare(b[0],a[0]));
+        for(int i=0;i<position.length;i++){
+           double time = ((double)(target-cars[i][0])/cars[i][1]);
+           if(stack.isEmpty() || time > stack.peek()){
+            stack.push(time);
+           }
+           
+        }
+        return stack.size();
+    }
+}
