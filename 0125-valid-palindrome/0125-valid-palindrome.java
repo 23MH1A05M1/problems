bc5bd3f8c940d0022d72af1cd07031fe957d2075
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-08-11 11:51:58 +0530

    Time: 15 ms (19%), Space: 47.7 MB (10%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       s=s.toLowerCase();
-       s=s.replaceAll("[^A-Za-z0-9]","");
-     StringBuilder sb = new StringBuilder(s);
-      sb.reverse();
-       String str =sb.toString();
-       if(s.equals(str)){
-        return true;
-       }
-       return false;
-       
+        s=s.toLowerCase();
+        s = s.replaceAll("[^A-Za-z0-9]","");
+        StringBuilder sb = new StringBuilder(s);
+        sb = sb.reverse();
+        String str = new String(sb);
+        if(s.equals(str)){
+            return true;
+        }
+        return false;
     }
 }
