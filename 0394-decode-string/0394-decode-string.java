0966e4eba81f049152b88f3b177f1f4b0b420964
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-08-16 22:03:23 +0530

    Time: 3 ms (22.94%), Space: 42.9 MB (40.45%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer>numStack=new Stack<>();
+        Stack<String>strStack = new Stack<>();
+        int num=0;
+        String current="";
+        for(char ch:s.toCharArray()){
+            if(Character.isDigit(ch)){
+                num=num*10+(ch-'0');
+            }
+            else if(ch=='['){
+                 numStack.add(num);
+                 strStack.add(current);
+                 num=0;
+                 current="";
+            }
+            else if(ch==']'){
+                int repeat = numStack.pop();
+                String previous = strStack.pop();
+                StringBuilder temp = new StringBuilder(previous);
+                for(int i=0;i<repeat;i++){
+                    temp.append(current);
+                }
+                current = temp.toString();
+            }
+            else{
+                current+=ch;
+            }
+        }
+        return current;
+    }
+}
