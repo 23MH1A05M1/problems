f301b1b83b4c89fa09ca8dc9d22a469ac28095b8
DWARAMPUDI BHAGYASRI <23MH1A05M1@acoe.edu.in>
2026-09-04 21:12:16 +0530

    Time: 1 ms (99.79%), Space: 46.4 MB (32.13%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int ans =-1;
+        int prefix[] = new int[nums.length];
+        int suffix[] = new int[nums.length];
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            prefix[i]=Math.max(prefix[i-1],nums[i]);
+        }
+        suffix[nums.length-1]=nums[nums.length-1];
+        for(int i=nums.length-2;i>=0;i--){
+            suffix[i]=Math.min(suffix[i+1],nums[i]);
+        }
+        for(int i=0;i<prefix.length;i++){
+            if(prefix[i]-suffix[i]<=k){
+                ans=i;
+                break;
+            }
+        }
+        return ans;
+    }
+}
